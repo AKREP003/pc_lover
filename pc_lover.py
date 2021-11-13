0db30771d9c94999ada0afa9471e91f0a1bc56d4
AKREP003 <59123551+AKREP003@users.noreply.github.com>
2021-11-13 23:32:47 +0300

    Initial commit

---
+++ pc_lover.py
@@ -0,0 +1,16 @@
+
+from itertools import permutations
+
+
+def Convert(string):
+    list1=[]
+    list1[:0]=string
+    return list1
+
+x = "LOL"
+
+x = Convert(x)
+
+while 1:
+
+    x = Convert(str(list(permutations(x, len(x)))))
